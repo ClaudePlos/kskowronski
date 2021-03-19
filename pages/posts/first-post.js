33d16e36a126772d3ac5828b02b9ed69334b359a
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-03-19 14:14:46 +0100

    q09

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -5,7 +5,7 @@ export default function FirstPost() {
         <>
           <h1>First Post</h1>
           <h2>
-            <Link href="/">
+            <Link href="/kskowronski/">
               <a>Back to home</a>
             </Link>
           </h2>
